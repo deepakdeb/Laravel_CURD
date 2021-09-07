d4522f5bf38d6abde48c8d4aa61ada10b15ae1fd
Deepak3642 <deepakdeb001@gmail.com>
2021-09-08 01:30:36 +0600

    Chart added

--- resources/views/jsn/custom_search.blade.php
+++ resources/views/jsn/custom_search.blade.php
@@ -1,7 +1,7 @@
 <html>
  <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel 5.8 - Individual Column Search in Datatables using Ajax</title>
+  <title>Stock Data</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
