b3e36911f2f73e3e5f417989ffc6e6d3e656ed6e
Deepak3642 <deepakdeb001@gmail.com>
2021-09-08 16:35:44 +0600

    Chart added

--- app/Http/Controllers/jsonController.php
+++ app/Http/Controllers/jsonController.php
@@ -33,16 +33,24 @@ class jsonController extends Controller
         $date = Stock::select('date')->orderBy('date', 'ASC')->get()->toArray();
         $close = Stock::select('close')->orderBy('date', 'ASC')->get()->toArray();
         $trade_code = Stock::select('trade_code')->orderBy('date', 'ASC')->get()->toArray();
+        $high= Stock::select('high')->orderBy('date', 'ASC')->get()->toArray();
+        $low = Stock::select('low')->orderBy('date', 'ASC')->get()->toArray();
+        $open = Stock::select('open')->orderBy('date', 'ASC')->get()->toArray();
         $volume = Stock::select('volume')->orderBy('date', 'ASC')->get()->toArray();
 
         $stock = new Stock;
-        $stock->date = $date;
-        $stock->close = $close;
+        $stock->date = $date;        
         $stock->trade_code = $trade_code;
+        $stock->high = $high;
+        $stock->low = $low;
+        $stock->open = $open;
+        $stock->close = $close;
         $stock->volume = $volume;
 
+        $data['stocks'] = Stock::orderBy('id','desc');
+
         $trade_code = Stock::select('trade_code')->groupBy('trade_code')->get();
-        return view('jsn.custom_search', compact('trade_code','stock'));
+        return view('jsn.custom_search',  compact('trade_code','stock',$data));
         }
     
 }
